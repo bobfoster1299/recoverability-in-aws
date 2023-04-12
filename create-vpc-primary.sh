be459455762bd66c6d59e65d7c6c7017f77c9a2d
Robert Foster <robert.foster2@nhm.ac.uk>
2023-04-12 22:10:25 +0100

    update

--- create-vpc-primary.sh
+++ create-vpc-primary.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
   --stack-name=Primary \
   --template-body=file://vpc.yml \
-  --parameters=file://params-primary.yml \
+  --parameters=file://params-primary.json \
   --region=us-east-1 
